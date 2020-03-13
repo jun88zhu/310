a75467948d6fa32b53792e8a2d6f7f4f1f0d4723
JUN ZHU <jun88zhu@hotmail.com>
2020-03-13 01:54:21 -0500

    change

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='SuperSecretKey'
+
+
+class MemberForm(FlaskForm):
+    first_name = StringField('First Name:', validators=[DataRequired()])
+    last_name = StringField('Last Name:', validators=[DataRequired()])
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', pageTitle='lakers team member')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/add_member', methods=['GET','POST'])
+def add_member():
+    form = MemberForm()
+    if form.validate_on_submit():
+        return "<h2> Los Angeles Lakers team member name is {0} {1}".format(form.first_name.data, form.last_name.data)
+
+    return render_template('add_member.html', form=form, pageTitle='Add A New Member')
 
 if __name__ == '__main__':
     app.run(debug=True)
